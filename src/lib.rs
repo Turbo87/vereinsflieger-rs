5597e7b507e882acc63b827d92f5783ea6092865
Tobias Bieniek <tobias@bieniek.cloud>
2025-01-13 22:26:29 +0100

    Save `access_token` inside `Client` struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,36 @@
 pub struct Client {
+    #[allow(dead_code)]
     client: reqwest::Client,
+    access_token: String,
 }
 
 impl Client {
-    pub fn new() -> Self {
+    pub async fn new() -> Result<Self, reqwest::Error> {
         let client = reqwest::Client::new();
-        Self { client }
+        let access_token = get_access_token(&client).await?;
+        Ok(Self {
+            client,
+            access_token,
+        })
     }
 
-    pub async fn get_access_token(&self) -> Result<String, reqwest::Error> {
-        #[derive(Debug, serde::Deserialize)]
-        struct Response {
-            #[serde(rename = "accesstoken")]
-            access_token: String,
-        }
-
-        let response = self
-            .client
-            .get("https://www.vereinsflieger.de/interface/rest/auth/accesstoken")
-            .send()
-            .await?
-            .error_for_status()?;
-
-        Ok(response.json::<Response>().await?.access_token)
+    pub fn access_token(&self) -> &str {
+        &self.access_token
     }
 }
 
-impl Default for Client {
-    fn default() -> Self {
-        Self::new()
+async fn get_access_token(client: &reqwest::Client) -> Result<String, reqwest::Error> {
+    #[derive(Debug, serde::Deserialize)]
+    struct Response {
+        #[serde(rename = "accesstoken")]
+        access_token: String,
     }
+
+    let response = client
+        .get("https://www.vereinsflieger.de/interface/rest/auth/accesstoken")
+        .send()
+        .await?
+        .error_for_status()?;
+
+    Ok(response.json::<Response>().await?.access_token)
 }
